658d7a0a46c937d2b0ab5f2c104f27e0aa218948
XuanSon <hson512475@gmail.com>
2024-02-22 17:03:23 +0700

    done ass02

---
+++ Ass02Solution/SalesWinApp/frmMyOrder.cs
@@ -0,0 +1,39 @@
+﻿using BusinessObject;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesWinApp
+{
+    public partial class frmMyOrder : Form
+    {
+        public MemberObject memberObject { get; set; }
+        public IOrderDetailRepository orderDetailRepository = new OrderDetailRepository();
+        public frmMyOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMyOrder_Load(object sender, EventArgs e)
+        {
+            dgvMyOrder.DataSource = orderDetailRepository.GetOrdersDetail();
+            List<OrderDetailObject> list = orderDetailRepository.GetOrdersDetail();
+            foreach(OrderDetailObject orderDetail in list)
+            {
+                tbTotalOrder.Text = orderDetail.Quantity * orderDetail.UnitPrice + "";
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ass02Solution/SalesWinApp/frmViewOrder.cs
@@ -0,0 +1,92 @@
+﻿using BusinessObject;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SalesWinApp
+{
+    public partial class frmCart : Form
+    {
+        public IProductRepository productRepository = new ProductRepository();
+        private List<ProductObject> productsList;
+        private List<ProductObject> productsListSearch;
+        public MemberObject memberObject { get; set; }
+        BindingSource bindingSource;
+        public frmCart()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCart_Load(object sender, EventArgs e)
+        {
+            cbSearch.SelectedIndex = 0;
+            LoadProductList();
+            dgvProduct.CellDoubleClick += DgvProduct_CellDoubleClick;
+        }
+
+        private void LoadProductList()
+        {
+            try
+            {
+                var productList = productRepository.GetProductObject();
+
+                if (productList != null && productList.Count > 0)
+                {
+                    this.productsList = productList;
+
+                    bindingSource = new BindingSource();
+                    bindingSource.DataSource = productsList;
+
+                    
+
+                    dgvProduct.DataSource = null;
+                    dgvProduct.DataSource = bindingSource;
+
+                    btFilter.Enabled = true;
+                }
+                else
+                {
+                    btFilter.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error when load list");
+            }
+        }
+
+        private void DgvProduct_CellDoubleClick(object? sender, DataGridViewCellEventArgs e)
+        {
+            frmProductOrder frmProductOrder = new frmProductOrder()
+            {
+                memberObject = memberObject,
+                ProductObject = productsList[e.RowIndex],
+                Text = "Order product"
+            };
+            if (frmProductOrder.ShowDialog() == DialogResult.OK)
+            {
+                LoadProductList();
+                bindingSource.Position = bindingSource.Count - 1;
+            }
+        }
+
+        private void btViewCart_Click(object sender, EventArgs e)
+        {
+           frmMyOrder frmMyOrder = new frmMyOrder()
+           {
+                memberObject = memberObject
+            };
+            this.Hide();
+            frmMyOrder.ShowDialog();
+            this.Show();
+        }
+    }
+}
